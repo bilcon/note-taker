befe741fbe7c3d353bfba1f734257f4063c4b279
Bill Morris <bilcon@swbell.net>
2021-12-15 15:17:32 -0600

    updated code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,42 +1,25 @@
-const router = require('express').Router();
+const router = require("express").Router();
+const notes = require("../db/notes.js");
 
-const store = require('../db/store');
+router.get("/notes", (req, res) => {
+  notes
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(400).json(err));
+});
 
-// requesting the existing notes
+router.post("/notes", (req, res) => {
+  notes
+    .addNotes(req.body)
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(400).send("No new notes.").json(err));
+});
 
-router.get('/notes', (req, res) => {
-    store
-        .getNotes()
-        .then(notes => {
-            res.json(notes)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-})
-
-// posting note function route 
-
-router.post('/notes', (req, res) => {
-    console.log(req.body)
-    store
-        .addNote(req.body)
-        .then(note => {
-            res.json(note)
-        })
-        .catch(err => {
-            res.status(500).json(err)
-        })
-})
-
-
-// delete note function route
-
-router.delete('/notes/:id', (req, res) => {
-    store
-        .removeNote(req.params.id)
-        .then(() => res.json({ ok: true }))
-        .catch(err => res.status(500).json(err))
-})
+router.delete("/notes/:id", function (req, res) {
+  notes
+    .removeNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(400).json(err));
+});
 
 module.exports = router;
